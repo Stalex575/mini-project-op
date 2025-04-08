599b019f28679101306461d4e12fbbc7328c1af3
Oleh Hutsuliak <oleg.hutsuliak@gmail.com>
2025-04-08 23:37:17 +0300

    Created an endpoint to receive and write new obstacles to the csv file

--- mini-project-op-back/main.py
+++ mini-project-op-back/main.py
@@ -1,8 +1,10 @@
 """Main"""
 
+import csv
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+import osmnx as ox
 from routing import get_route, load_ukraine_graph
 
 @asynccontextmanager
@@ -44,3 +46,28 @@ async def route(request: Request) -> dict:
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) from e
     return {"route": route_coords}
+
+@app.post('/obstacles')
+async def obstacles(request: Request) -> None:
+    """
+    Obstacles endpoint.
+
+    :param request: Request, A frontend request with obstacles coordinates [[start, end], ...].
+    """
+    try:
+        data = await request.json()
+        graph = app.state.ukraine_graph
+
+        node_ids = [
+            ox.distance.nearest_nodes(graph, lon, lat)
+            for lat, lon in data['obstacles']
+        ]
+
+        with open('obstacles.csv', 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+
+            for node_id in node_ids:
+                writer.writerow([node_id])
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
