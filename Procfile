43a117474c1f3c6ab667213a5bc1f76944ef9ab3
Stalex575 <stadnick007@gmail.com>
2025-04-28 15:15:20 +0300

    frontend refactor

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: uvicorn mini_project_op_back.main:app --host=0.0.0.0 --port=$PORT
--- mini_project_op_back/test.py
+++
@@ -1,4 +0,0 @@
-import osmnx as ox
-
-ukraine_graph = ox.graph_from_place("Lviv, Ukraine", network_type="drive")
-ox.save_graphml(ukraine_graph, "ukraine_drive.graphml")
